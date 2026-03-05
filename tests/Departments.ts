eb8e3ffcdfd55887d8c0a6bc559dc94e5585a58e
CED <cedrix.busa@gmail.com>
2026-03-05 21:07:16 +0800

    03052026 - Added Departments Set of Test Cases - verify Department navigation

---
+++ tests/Departments.ts
@@ -0,0 +1,19 @@
+import {test,Page,Locator,expect} from '@playwright/test';
+
+export class Departments {
+    readonly page: Page;
+    readonly CommunittyMenuDropdown: Locator;
+    readonly DepartmentsMenuOption: Locator;
+
+
+    constructor(page: Page){
+        this.page = page;
+        this.CommunittyMenuDropdown = page.getByText('Community');
+        this.DepartmentsMenuOption = page.getByRole('link', { name: 'Departments' });
+
+    }
+    async GoToDepartmentsPage(){
+        await this.CommunittyMenuDropdown.click();
+        await this.DepartmentsMenuOption.click();
+    }
+}
